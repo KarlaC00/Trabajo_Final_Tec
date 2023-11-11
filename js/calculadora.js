b0f77904d1cebc5b955e3e9d410d686cb654db30
KarlaC00 <karvalentinap@gmail.com>
2023-11-11 02:26:41 -0500

    YA_ACABE_EL_HOME

--- js/calculadora.js
+++ js/calculadora.js
@@ -2,15 +2,12 @@ function calcularIMC() {
     var peso = parseFloat(document.getElementById("peso").value);
     var altura = parseFloat(document.getElementById("altura").value);
 
-    // Verifica que los valores sean válidos
     if (isNaN(peso) || isNaN(altura) || altura <= 0 || peso <= 0) {
       alert("Por favor, ingrese valores válidos para peso y altura.");
       return;
     }
 
-    // Calcula el IMC
     var imc = peso / (altura ** 2);
     
-    // Muestra el resultado en la página
     document.getElementById("resultadoIMC").textContent = imc.toFixed(2);
   }
